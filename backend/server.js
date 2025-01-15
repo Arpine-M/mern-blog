f337d33d3897aab656b186d875888e3f36e20798
AM <malerischam@yahoo.com>
2025-01-16 02:32:37 +0400

    making requests from frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const Post = require("./models/Post/Post");
 const connectDB = require("./utils/connectDB");
@@ -9,6 +10,12 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.post('/api/v1/posts/create', async (req, res) => {
     try {

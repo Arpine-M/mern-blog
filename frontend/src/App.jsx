cfb1ce69309cdea7c77f7ccb211535c4d179f889
AM <malerischam@yahoo.com>
2025-01-31 02:10:25 +0400

    Some updates in the components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import CreatePost from './components/Posts/CreatePost'
-import PostsList from './components/PostsList'
-import PublicNavbar from './components/Navbar/PublicNavbar'
-import HomePage from './components/Home/HomePage'
-import {Link} from 'react-router-dom'
-import UpdatePost from './components/Posts/UpdatePost'
+import CreatePost from "./components/Posts/CreatePost";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import { Link } from "react-router-dom";
+import UpdatePost from "./components/Posts/UpdatePost";
+import PostsList from "./components/Posts/PostsList";
+import Home from "./components/Home/Home";
+import PostDetails from "./components/Posts/PostDetails";
 function App() {
- 
-
   return (
     <BrowserRouter>
-    <PublicNavbar/>
-    <Routes>
-      <Route element={<HomePage />} path="/" />
-      <Route element={<CreatePost />} path="/create-post" />
-      <Route element={<PostsList />} path="/lists" />
-      <Route element={<UpdatePost />} path="/posts/:postId" />
+      <PublicNavbar />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<CreatePost />} path="/create-post" />
+        <Route element={<PostsList />} path="/lists" />
+        <Route element={<PostDetails />} path="/posts/:postId" />
+        <Route element={<UpdatePost />} path="/posts/:postId" />
 
-
-     
-    </Routes>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

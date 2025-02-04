a7e7c4181aaff9450f87d84bd69799d052e68f04
AM <malerischam@yahoo.com>
2025-02-05 02:36:02 +0400

    google oauth

--- backend/utils/passport-config.js
+++ backend/utils/passport-config.js
@@ -2,8 +2,12 @@ const passport = require("passport");
 const User = require("../models/User/User");
 const bcrypt = require("bcryptjs");
 const LocalStrategy = require("passport-local").Strategy;
+const JWTStrategy = require("passport-jwt").Strategy; 
+const ExtractJWT = require("passport-jwt").ExtractJwt; 
+const GoogleStrategy = require("passport-google-oauth20");
 
 //! Configure passport local strategy
+
 
 passport.use(
   new LocalStrategy(
@@ -28,6 +32,32 @@ passport.use(
       }
     }
   )
+);
+
+const options = {
+  jwtFromRequest: ExtractJWT.fromExtractors([(req)=>{
+    let token = null;
+    if(req && req.cookies){
+      token = req.cookies["token"];
+      return token;
+    }
+    
+  }]),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JWTStrategy(options, async (userDecoded, done) => {
+    try {
+      const user  = await User.findById(userDecoded.id)
+      if(!user){
+        return done(null, user);
+      } else
+      return done(null, false);
+    } catch (error) {
+      return done(error, false);
+    }
+  })
 );
 
 module.exports = passport;
